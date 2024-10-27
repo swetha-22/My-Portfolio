9b3c7631c1658c1df13c8bcaaec90bce4bd0fc17
Swetha Paspunuri <swethapaspunuri227@gmail.com>
2024-10-27 13:36:56 +0530

    Added Download Resume Button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import { AppContext } from "../App.jsx";
 import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { AiOutlineDownload } from 'react-icons/ai';
 
 import ToggleTheme from "../components/ToggleTheme.jsx";
 
@@ -11,6 +12,8 @@ import Footer from "../components/sections/Footer.jsx";
 import EducationAndExperience from "../components/sections/EducationAndExperience.jsx";
 import Skills from "../components/sections/Skills.jsx";
 
+const pdfFilePath = '/assets/Swetha_Paspunuri_Java.pdf';
+
 function Homepage() {
   const { theme, switchTheme } = useContext(AppContext);
   const location = useLocation();
@@ -19,11 +22,29 @@ function Homepage() {
     window.HSStaticMethods.autoInit();
   }, [location.pathname]);
 
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = pdfFilePath;
+    link.download = 'resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }; 
+
   return (
     <div className="bg-zinc-100 dark:bg-zinc-900">
-      <div className="xl:w-[1200px] md:mx-auto h-full border-x border-zinc-100 dark:border-zinc-800 bg-white dark:bg-zinc-950">
+      <div className="md:pt-12 xl:w-[1200px] md:mx-auto h-full border-x border-zinc-100 dark:border-zinc-800 bg-white dark:bg-zinc-950">
         <ToggleTheme switchTheme={switchTheme} />
-
+        <div className="absolute ml-5 md:ml-10 md:top-12">
+          <button onClick={handleDownload} className="flex w-[170px] p-1 px-3 py-3 text-primaryDark border border-primaryDark hover:bg-primary hover:text-white hover:border-primary dark:bg-primaryLightest font-medium transition-all duration-300 rounded-lg">
+            <div className=" mx-2 text-2xl">
+              <AiOutlineDownload />
+            </div>
+            <div>
+              Download CV
+            </div>
+          </button>
+        </div>
         <Hero />
         <Projects />
 				<EducationAndExperience />
